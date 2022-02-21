f2ead497529199bcac37825412c094cb0b63ff7c
Gabriel Moya <gabriel.mm@lyncas.net>
2022-02-21 11:38:02 -0300

    Responsive dashboard and listing

--- frontend/src/javascript/menuToggle.js
+++ frontend/src/javascript/menuToggle.js
@@ -1,6 +1,8 @@
 const iconMenuToggle = document.getElementById('toggle');
 const iconCloseMenu = document.getElementById('close');
+const container = document.querySelector('.container');
 const sidebar = document.querySelector(".sidebar");
+const widthScreen = window.screen.width;
 
 iconCloseMenu.addEventListener("click", () => {
   hideMenu();
@@ -20,9 +22,16 @@ iconMenuToggle.addEventListener("click", () => {
 
 function displayMenu() {
   sidebar.style.display="block";
+
+  if(widthScreen > 800) {
+    container.style.width = "calc(100% - 300px)"
+  } else {
+    container.style.width = "calc(100% - 250px)"
+  }
 }
 
 function hideMenu() {
   sidebar.style.display="none";
   iconMenuToggle.style.marginLeft = "30px";
+  container.style.width = "100%"
 }

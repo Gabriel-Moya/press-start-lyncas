60f51c45080cefc9f3486fa0e8fd3671899f7108
Gabriel Moya <gabriel.mm@lyncas.net>
2022-02-23 12:04:46 -0300

    Correct responsive menu

--- frontend/src/javascript/menuToggle.js
+++ frontend/src/javascript/menuToggle.js
@@ -1,8 +1,7 @@
 const iconMenuToggle = document.getElementById('toggle');
 const iconCloseMenu = document.getElementById('close');
 const container = document.querySelector('.container');
-const sidebar = document.querySelector(".sidebar");
-const widthScreen = window.screen.width;
+const sidebar = document.querySelector('.sidebar');
 
 iconCloseMenu.addEventListener("click", () => {
   hideMenu();
@@ -22,16 +21,47 @@ iconMenuToggle.addEventListener("click", () => {
 
 function displayMenu() {
   sidebar.style.display="block";
-
-  if(widthScreen > 800) {
-    container.style.width = "calc(100% - 300px)"
+  
+  if (window.matchMedia("(min-width: 801px)").matches) {
+    container.style.width = "calc(100% - 300px)";
   } else {
-    container.style.width = "calc(100% - 250px)"
+    container.style.width = "100%";
   }
 }
 
 function hideMenu() {
-  sidebar.style.display="none";
-  iconMenuToggle.style.marginLeft = "30px";
+  sidebar.style.display = "none";
   container.style.width = "100%"
 }
+
+const screenSize = function() {
+
+  const screenResize = function(e) {
+    const screenSize = window.matchMedia("(max-width: 801px)");
+    if (screenSize.matches) {
+      sidebar.style.display = "none"
+      container.style.width = "100%";
+    } else {
+      sidebar.style.display = "block"
+      container.style.width = "calc(100% - 300px)";
+    };
+  }
+
+  window.addEventListener('resize', screenResize, false);
+}
+
+document.addEventListener('DOMContentLoaded', screenSize, false);
+
+/* function sidebarSize(screenSize) {
+  if (screenSize.matches) {
+    sidebar.style.display = "none"
+    container.style.width = "100%";
+  } else {
+    sidebar.style.display = "block"
+    container.style.width = "calc(100% - 300px)";
+  }
+} */
+
+/* var screenSize = window.matchMedia("(max-width: 801px)");
+sidebarSize(screenSize);
+screenSize.addListener(sidebarSize); */
